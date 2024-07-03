fea8e192df2823f7a3095dcd859ca6e9395e14d7
won-hj <won.hyeockju@gmail.com>
2024-07-04 03:42:02 +0900

    commit test

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:iccas_test1/etc/quest.dart';
 import 'package:iccas_test1/etc/shop.dart';
 import 'package:iccas_test1/etc/statistics.dart';
 import 'package:iccas_test1/etc/survey.dart';
+import 'package:iccas_test1/game/bonus/play.dart';
 import 'package:iccas_test1/game/bonus/start.dart';
 import 'package:iccas_test1/game/story/start.dart';
 import 'package:iccas_test1/login.dart';
@@ -26,9 +27,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          //resizeToAvoidBottomInset: false,
-          body: BonusStart() //Login(),
-          ),
+        //resizeToAvoidBottomInset: false,
+        body: BonusPlay(), //BonusStart() //Login(),
+      ),
       initialRoute: "/BonusStart", //"/Login",
       routes: {
         "/Login": (context) => Login(),
