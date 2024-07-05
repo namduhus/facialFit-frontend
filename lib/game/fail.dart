793b3ac4b17696132116b1814c97e412747b4c9a
won-hj <won.hyeockju@gmail.com>
2024-07-05 20:40:29 +0900

    성공실패 리스트뷰 표현

--- lib/game/fail.dart
+++ lib/game/fail.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class StepFail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         Container(
           width: 478,
