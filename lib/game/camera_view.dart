045242fc8de105d187d27de4c72856e0792d1ef5
won-hj <won.hyeockju@gmail.com>
2024-07-15 17:26:13 +0900

    스테이지 완료 후 다시 스테이지 진입 시 카메라 재초기화

--- lib/game/camera_view.dart
+++ lib/game/camera_view.dart
@@ -11,12 +11,26 @@ class CameraView extends GetView<ScanController> {
 
   @override
   Widget build(BuildContext context) {
-    return GetX<ScanController>(builder: (controller) {
+    return GetX<ScanController>(initState: (state) {
+      controller.onInit();
+    }, builder: (controller) {
       if (!controller.isInitialized) {
         Logger().e("camera view error");
+        //3 33controller.onInit();
 
-        return Container();
+        return Scaffold(
+          body: SafeArea(
+            //width: Get.width,
+            //height: Get.height,
+            child: controller.isInitialized
+                ? CameraPreview(controller.cameraController)
+                : const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+          ),
+        );
       }
+
       return Stack(children: [
         Scaffold(
           body: SafeArea(
